41856705c6877da3c87a873d46f299dd15fd6f87
jkomonen <68486286+jkomonen@users.noreply.github.com>
2023-11-08 02:17:12 -0500

    code readability update

--- content.js
+++ content.js
@@ -1,20 +1,3 @@
-//~~~~~~~~~~//
-//~~~AVA~~~//
-//~~~~~~~~//
-// let imageList = [
-//     "https://biographymask.com/wp-content/uploads/2020/10/Ava-Rose.jpg",
-//     "https://pbs.twimg.com/profile_images/1281995898227249154/fkkWz0xs_400x400.jpg",
-//     "https://starsgab.com/wp-content/uploads/2020/10/75272067_451201595820583_5676222549784950576_n.jpg",
-//     "https://socialstarswiki.com/wp-content/uploads/2019/11/Ava-Rose.jpg",
-//     "https://bookingagentinfo.com/wp-content/uploads/2020/01/Ava-Rose-Contact-Information.jpg",
-//     "https://celebslifereel.com/wp-content/uploads/2020/02/Ava-Rose.jpg",
-//     "https://www.mixedarticle.com/wp-content/uploads/2019/11/Ava-Rose-Photo-819x1024.jpg",
-//     "https://i.scdn.co/image/ab67706c0000bebb8c6ce88323db5ecb98fd7cc4",
-//     "https://www.readingbio.com/wp-content/uploads/2020/02/Ava-Rose.jpeg",
-//     "https://dailyinet.com/wp-content/uploads/2020/10/AvaLogo_1200x1200-1024x1024.png",
-//     "https://preview.redd.it/sz41xnf29kl61.jpg?width=640&crop=smart&auto=webp&s=2d59ad93963c0606417bef27c61cb86546eb1058",
-//     "https://i.pinimg.com/236x/2f/40/ab/2f40ab6c3b19f36ee914a659da4207b2.jpg",
-//   ];
 let imageList = [
   "https://i.imgur.com/VV9RRt9.png",
   "https://i.imgur.com/6lbiaol.png",
